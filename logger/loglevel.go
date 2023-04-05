058474b0638cd6e7502b1b6bd4d1af5995e8683b
Bohdan Chepeliuk <bohdan.c@odeeo.io>
2023-04-06 01:57:22 +0300

    Add convertion for loglevel from string

--- logger/loglevel.go
+++ logger/loglevel.go
@@ -1,5 +1,12 @@
 package logger
 
+import (
+	"fmt"
+	"strings"
+
+	wherr "github.com/bohdanch-w/wheel/errors"
+)
+
 type LogLevel uint8
 
 const (
@@ -8,4 +15,39 @@ const (
 	Warn
 	Error
 	Fatal
+
+	invalid
+	errInvalidLogLevel = wherr.Error("loglevel is invalid")
 )
+
+func LogLevelFromString(str string) LogLevel {
+	switch strings.ToLower(strings.TrimSpace(str)) {
+	case "debug":
+		return Debug
+	case "info":
+		return Info
+	case "warn":
+		return Warn
+	case "error":
+		return Error
+	case "fatal":
+		return Fatal
+	case "":
+		return Info
+	}
+
+	return invalid
+}
+
+func (l *LogLevel) UnmarshalText(text []byte) error {
+	strLevel := string(text)
+
+	level := LogLevelFromString(strLevel)
+	if level == invalid {
+		return fmt.Errorf("%w: %q", errInvalidLogLevel, strLevel)
+	}
+
+	*l = level
+
+	return nil
+}
