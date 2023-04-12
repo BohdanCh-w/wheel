3d36f88989eeccfb3c1886435e19e65b5482b968
Bohdan Chepeliuk <bohdan.c@odeeo.io>
2023-04-12 23:37:02 +0300

    Remove extra logging in error mid

--- web/middleware/error.go
+++ web/middleware/error.go
@@ -5,19 +5,14 @@ import (
 	"errors"
 	"net/http"
 
-	whlogger "github.com/bohdanch-w/wheel/logger"
 	"github.com/bohdanch-w/wheel/web"
 	"github.com/bohdanch-w/wheel/web/api"
 )
 
-type ErrorMid struct {
-	Logger whlogger.Logger
-}
+type ErrorMid struct{}
 
 func (mid *ErrorMid) Wrap(h api.Handler) api.Handler {
 	f := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
-		transactionID := web.TransactionIDFromCtx(ctx)
-
 		err := h(ctx, w, r)
 		if err == nil {
 			return nil
@@ -28,11 +23,6 @@ func (mid *ErrorMid) Wrap(h api.Handler) api.Handler {
 		if !errors.As(err, &webErr) {
 			webErr.Err = err
 		}
-
-		mid.Logger.
-			WithTransaction(transactionID).
-			With("status", webErr.Status()).
-			Warnf("Request failed: %s", webErr.Error())
 
 		if webErr.Status() > 0 {
 			return web.Abort(w, webErr) // nolint: wrapcheck
