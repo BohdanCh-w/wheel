8053cb7c1f2c5a31dda2439266906eb285b9bf8f
Zumori <BohdanCh.w@gmail.com>
2023-05-20 18:06:25 +0300

    Update Symetric Map

--- symetric-map/symmap.go
+++ symetric-map/symmap.go
@@ -2,33 +2,99 @@
 // Validation on repeting keys is on user
 package symmap
 
+import (
+	"fmt"
+
+	"github.com/bohdanch-w/wheel/errors"
+)
+
 type Pair[L, R comparable] struct {
-	Left  L
-	Right R
+	L L
+	R R
 }
 
-func NewPairs[L, R comparable](pairs ...Pair[L, R]) SymMap[L, R] {
-	set := SymMap[L, R]{
+func New[L, R comparable](pairs ...any) *SymMap[L, R] {
+	mp, err := NewValues[L, R](pairs...)
+	if err != nil {
+		panic(err)
+	}
+
+	return mp
+}
+
+func NewPairs[L, R comparable](pairs ...Pair[L, R]) (*SymMap[L, R], error) {
+	mp := &SymMap[L, R]{
 		left:  make(map[L]R),
 		right: make(map[R]L),
 	}
 
 	for _, pair := range pairs {
-		set.left[pair.Left] = pair.Right
-		set.right[pair.Right] = pair.Left
+		if err := mp.Add(pair.L, pair.R); err != nil {
+			return nil, err
+		}
 	}
 
-	return set
+	return mp, nil
 }
 
+func NewValues[L, R comparable](pairs ...any) (*SymMap[L, R], error) {
+	const (
+		errInvalidParamNumber = errors.Error("invalid number of params")
+		errInvalidPair        = errors.Error("invalid pair")
+	)
+
+	if len(pairs)%2 != 0 {
+		return nil, fmt.Errorf("%w: %d", errInvalidParamNumber, len(pairs))
+	}
+
+	mp := &SymMap[L, R]{
+		left:  make(map[L]R),
+		right: make(map[R]L),
+	}
+
+	for i := 0; i < len(pairs); i += 2 {
+		left, lOk := pairs[i].(L)
+		right, rOk := pairs[i+1].(R)
+
+		if !lOk || !rOk {
+			return nil, fmt.Errorf(
+				"%w: received %v[%T] - %v[%T], expected [%T] - [%T]",
+				errInvalidPair,
+				pairs[i], pairs[i],
+				pairs[i+1], pairs[i+1],
+				left, right,
+			)
+		}
+
+		if err := mp.Add(left, right); err != nil {
+			return nil, err
+		}
+	}
+
+	return mp, nil
+}
+
+// SymMap is a standard golang map that can be used to map values in both directions.
 type SymMap[L, R comparable] struct {
 	left  map[L]R
 	right map[R]L
 }
 
-func (s SymMap[L, R]) Add(left L, right R) {
+func (s SymMap[L, R]) Add(left L, right R) error {
+	const errAlreadyMapped = errors.Error("duplicate value")
+
+	if right, ok := s.left[left]; ok {
+		return fmt.Errorf("%w: %v - %v", errAlreadyMapped, left, right)
+	}
+
+	if left, ok := s.right[right]; ok {
+		return fmt.Errorf("%w: %v - %v", errAlreadyMapped, left, right)
+	}
+
 	s.left[left] = right
 	s.right[right] = left
+
+	return nil
 }
 
 func (s SymMap[L, R]) Len() int {
@@ -39,10 +105,42 @@ func (s SymMap[L, R]) Empty() bool {
 	return len(s.left) == 0
 }
 
+func (s SymMap[L, R]) Left(right R) (L, bool) {
+	left, ok := s.right[right]
+
+	return left, ok
+}
+
+func (s SymMap[L, R]) Right(left L) (R, bool) {
+	right, ok := s.left[left]
+
+	return right, ok
+}
+
 func (s SymMap[L, R]) GetLeft(right R) L {
 	return s.right[right]
 }
 
 func (s SymMap[L, R]) GetRight(left L) R {
 	return s.left[left]
+}
+
+func (s SymMap[L, R]) Values() []Pair[L, R] {
+	pairs := make([]Pair[L, R], 0, s.Len())
+
+	for left, right := range s.left {
+		pairs = append(pairs, Pair[L, R]{L: left, R: right})
+	}
+
+	return pairs
+}
+
+func (s SymMap[L, R]) Map() map[L]R {
+	pairs := make(map[L]R, s.Len())
+
+	for left, right := range s.left {
+		pairs[left] = right
+	}
+
+	return pairs
 }
---
+++ symetric-map/symmap_test.go
@@ -0,0 +1,169 @@
+package symmap_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	symmap "github.com/bohdanch-w/wheel/symetric-map"
+)
+
+func TestSymetricMapNew_Success(t *testing.T) {
+	emptyMap, err := symmap.NewValues[string, int]()
+	require.NoError(t, err)
+
+	mp, err := symmap.NewValues[string, int]("test1", 1, "test2", 2)
+	require.NoError(t, err)
+
+	require.Equal(t, map[string]int{}, emptyMap.Map())
+	require.Equal(t, map[string]int{"test1": 1, "test2": 2}, mp.Map())
+}
+
+func TestSymetricMapNew_FailsNumberOfParams(t *testing.T) {
+	mp, err := symmap.NewValues[string, int]("test1", 1, "test2", 2, "test3")
+	require.Empty(t, mp)
+	require.EqualError(t, err, "invalid number of params: 5")
+}
+
+func TestSymetricMapNew_FailsType(t *testing.T) {
+	mp, err := symmap.NewValues[string, int]("test1", 1, 2.0, 2)
+	require.Empty(t, mp)
+	require.EqualError(t, err, "invalid pair: received 2[float64] - 2[int], expected [string] - [int]")
+}
+
+func TestSymetricMapNew_FailsDuplicate(t *testing.T) {
+	mp, err := symmap.NewValues[string, int]("test1", 1, "test1", 2)
+	require.Empty(t, mp)
+	require.EqualError(t, err, "duplicate value: test1 - 1")
+
+	mp, err = symmap.NewValues[string, int]("test1", 1, "test2", 1)
+	require.Empty(t, mp)
+	require.EqualError(t, err, "duplicate value: test1 - 1")
+
+	mp, err = symmap.NewValues[string, int]("test1", 1, "test1", 1)
+	require.Empty(t, mp)
+	require.EqualError(t, err, "duplicate value: test1 - 1")
+}
+
+func TestSymetricMapNewPairs_Success(t *testing.T) {
+	mp, err := symmap.NewPairs(symmap.Pair[string, int]{"test1", 1}, symmap.Pair[string, int]{"test2", 2})
+	require.NoError(t, err)
+
+	require.Equal(t, map[string]int{"test1": 1, "test2": 2}, mp.Map())
+}
+
+func TestSymetricMapNewPairs_FailsDuplicate(t *testing.T) {
+	mp, err := symmap.NewPairs(symmap.Pair[string, int]{"test1", 1}, symmap.Pair[string, int]{"test1", 2})
+	require.Empty(t, mp)
+	require.EqualError(t, err, "duplicate value: test1 - 1")
+
+	mp, err = symmap.NewPairs(symmap.Pair[string, int]{"test1", 1}, symmap.Pair[string, int]{"test2", 1})
+	require.Empty(t, mp)
+	require.EqualError(t, err, "duplicate value: test1 - 1")
+
+	mp, err = symmap.NewPairs(symmap.Pair[string, int]{"test1", 1}, symmap.Pair[string, int]{"test1", 1})
+	require.Empty(t, mp)
+	require.EqualError(t, err, "duplicate value: test1 - 1")
+}
+
+func TestSymetricMapMust_Success(t *testing.T) {
+	require.NotPanics(t, func() {
+		mp := symmap.New[string, int]("test1", 1, "test2", 2)
+
+		require.Equal(t, map[string]int{"test1": 1, "test2": 2}, mp.Map())
+	})
+}
+
+func TestSymetricMapMust_Fail(t *testing.T) {
+	require.PanicsWithError(t, "invalid number of params: 5", func() {
+		symmap.New[string, int]("test1", 1, "test2", 2, "test3")
+	})
+}
+
+func TestSymetricMapLen(t *testing.T) {
+	emptyMap := symmap.New[string, int]()
+	mp := symmap.New[string, int]("test1", 1)
+
+	require.Equal(t, 0, emptyMap.Len())
+	require.Equal(t, 1, mp.Len())
+}
+
+func TestSymetricMapEmpty(t *testing.T) {
+	emptyMap := symmap.New[string, int]()
+	mp := symmap.New[string, int]("test1", 1)
+
+	require.True(t, emptyMap.Empty())
+	require.False(t, mp.Empty())
+}
+
+func TestSymetricMapAdd_Success(t *testing.T) {
+	mp := symmap.New[string, int]()
+
+	require.Equal(t, 0, mp.Len())
+
+	err := mp.Add("test1", 1)
+	require.NoError(t, err)
+
+	err = mp.Add("test2", 2)
+	require.NoError(t, err)
+
+	require.Equal(t, map[string]int{"test1": 1, "test2": 2}, mp.Map())
+}
+
+func TestSymetricMapAdd_Fail(t *testing.T) {
+	mp := symmap.New[string, int]()
+
+	require.Equal(t, 0, mp.Len())
+
+	err := mp.Add("test1", 1)
+	require.NoError(t, err)
+
+	err = mp.Add("test1", 1)
+	require.EqualError(t, err, "duplicate value: test1 - 1")
+}
+
+func TestSymetricMapValues(t *testing.T) {
+	emptyMap := symmap.New[string, int]()
+	mp := symmap.New[string, int]("test1", 1, "test2", 2)
+
+	require.ElementsMatch(t, []symmap.Pair[string, int]{}, emptyMap.Values())
+	require.ElementsMatch(t, []symmap.Pair[string, int]{{L: "test1", R: 1}, {L: "test2", R: 2}}, mp.Values())
+}
+
+func TestSymetricMapRight(t *testing.T) {
+	mp := symmap.New[string, int]("test1", 1)
+
+	right, ok := mp.Right("test1")
+	require.True(t, ok)
+	require.Equal(t, 1, right)
+
+	right, ok = mp.Right("test2")
+	require.False(t, ok)
+	require.Empty(t, right)
+}
+
+func TestSymetricMapLeft(t *testing.T) {
+	mp := symmap.New[string, int]("test1", 1)
+
+	left, ok := mp.Left(1)
+	require.True(t, ok)
+	require.Equal(t, "test1", left)
+
+	left, ok = mp.Left(2)
+	require.False(t, ok)
+	require.Empty(t, left)
+}
+
+func TestSymetricMapGetRight(t *testing.T) {
+	mp := symmap.New[string, int]("test1", 1)
+
+	require.Equal(t, 1, mp.GetRight("test1"))
+	require.Equal(t, 0, mp.GetRight("test2"))
+}
+
+func TestSymetricMapGetLeft(t *testing.T) {
+	mp := symmap.New[string, int]("test1", 1)
+
+	require.Equal(t, "test1", mp.GetLeft(1))
+	require.Equal(t, "", mp.GetLeft(2))
+}
